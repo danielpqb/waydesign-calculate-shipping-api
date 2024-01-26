abfdaee26d7abddf14e3db2c832a9ea2d2941b94
danielpqb <danielpqb@gmail.com>
2024-01-26 13:09:36 -0300

    fix

--- src/controllers/shippingController.js
+++ src/controllers/shippingController.js
@@ -37,7 +37,7 @@ async function calculateShippingValue(req, res) {
       break;
   }
 
-  console.log("@RESPONSE: " + JSON.stringify(res_obj));
+  console.log("@RESPONSE: " + JSON.stringify([res_obj]));
 
   try {
     return res.status(200).send([res_obj]);
